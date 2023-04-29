66c117af41b7ba4e67dd508ad9aef3e08b0144de
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2023-04-29 21:20:30 +0300

    refactoring chats scheme (#4)
    * refactoring chats scheme

--- alembic/versions/1_create_chat_user_9c10d536a11c_.py
+++ alembic/versions/1_create_chat_user_9c10d536a11c_.py
@@ -5,9 +5,9 @@ Revises:
 Create Date: 2023-04-22 12:18:30.912601
 
 """
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "9c10d536a11c"
