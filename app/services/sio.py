3cab7d72ad05313800b97bd0fd9e469f81ae33e7
Georgii Alzh <124917697+AlzhGeorgii@users.noreply.github.com>
2023-05-28 17:54:22 +0300

    add uid checking to sio connect handler (#13)
    * add uid checking to sio connect handler
    
    * deleted prints
    
    * changed response for bad request

--- app/services/sio.py
+++ app/services/sio.py
@@ -1,17 +1,21 @@
 from loguru import logger
+from sqlalchemy import select
 from starlette.datastructures import Headers
 
-from app import config
+from app import config, db
+from app.db.registry import registry
 from app.schemas.sio import SioEvents
 
 
 async def connect(sid: str, environ: dict) -> str | None:
-    print(environ["asgi.scope"]["headers"])
     headers = Headers(raw=environ["asgi.scope"]["headers"])
-    print(headers)
     logger.debug(f"headers - {headers}")
     user_id = headers.get(config.application.user_header_name)
     if not user_id:
         return SioEvents.USER_MISSING
+    query = select(db.User).where(db.User.uid == user_id)
+    async with registry.session() as session:
+        if not (await session.execute(query)).scalar():
+            return SioEvents.USER_MISSING
     logger.debug(f"User id - {user_id}")
     logger.info(f"Connect user: {user_id} with sid: {sid}")
