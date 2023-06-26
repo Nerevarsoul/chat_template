64b2f84d7af7447eafb60ecd4214b3db3bad84aa
Georgii Alzhirov <alzh.george@gmail.com>
2023-06-26 18:23:17 +0000

    added test_get_chat_recipients_from_non_existent_chat

--- tests/app/api/chats/test_get_chat_recipients.py
+++ tests/app/api/chats/test_get_chat_recipients.py
@@ -73,3 +73,18 @@ async def test_get_chat_recipients(client: "AsyncClient", user_db_f, chat_relati
             assert recipient["user_uid"] == str(user_2.uid)
             assert recipient["state"] == ChatState.ACTIVE
             assert recipient["user_role"] == ChatUserRole.USER
+
+
+@pytest.mark.usefixtures("clear_db")
+async def test_get_chat_recipients_from_non_existent_chat(
+    client: "AsyncClient", user_db_f, chat_relationship_db_f, chat_db_f
+) -> None:
+    user_1 = await user_db_f.create()
+
+    response = await client.get(
+        app.other_asgi_app.url_path_for("get_chat_recipients"),
+        headers={config.application.user_header_name: str(user_1.uid)},
+        params={"chat_id": 1},
+    )
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
