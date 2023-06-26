28378584279129c614745beeb0921cfb15256db8
Georgii Alzhirov <alzh.george@gmail.com>
2023-06-26 19:33:15 +0000

    moved authorization tests to test_api_routers_authorization

---
+++ tests/app/api/chats/test_api_routers_authorization.py
@@ -0,0 +1,46 @@
+import uuid
+from typing import TYPE_CHECKING
+
+import pytest
+from fastapi import status
+
+from app import config
+from app.main import app
+
+if TYPE_CHECKING:
+    from httpx import AsyncClient
+
+
+@pytest.mark.parametrize(
+    "url,method",
+    [
+        ("create_chat", "POST"),
+        ("get_chat_list", "GET"),
+        ("get_chat_recipients", "GET"),
+    ],
+)
+async def test_request_without_user_id(client: "AsyncClient", url, method: str) -> None:
+    response = await client.request(method=method, url=app.other_asgi_app.url_path_for(url))
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {"detail": "User not found"}
+
+
+@pytest.mark.parametrize(
+    "url,method",
+    [
+        ("create_chat", "POST"),
+        ("get_chat_list", "GET"),
+        ("get_chat_recipients", "GET"),
+    ],
+)
+async def test_request_with_user_not_exist(client: "AsyncClient", url: str, method: str) -> None:
+    user_uid = str(uuid.uuid4())
+    response = await client.request(
+        method=method,
+        url=app.other_asgi_app.url_path_for(url),
+        headers={config.application.user_header_name: user_uid},
+    )
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {"detail": "User not found"}
--- tests/app/api/chats/test_get_chat_recipients.py
+++ tests/app/api/chats/test_get_chat_recipients.py
@@ -1,4 +1,3 @@
-import uuid
 from typing import TYPE_CHECKING
 
 import pytest
@@ -10,22 +9,6 @@ from app.main import app
 
 if TYPE_CHECKING:
     from httpx import AsyncClient
-
-
-async def test_get_chat_recipients_without_user_id(client: "AsyncClient") -> None:
-    response = await client.get(app.other_asgi_app.url_path_for("get_chat_recipients"))
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert response.json() == {"detail": "User not found"}
-
-
-async def test_get_chat_recipients_user_not_exist(client: "AsyncClient") -> None:
-    user_uid = str(uuid.uuid4())
-    response = await client.get(
-        app.other_asgi_app.url_path_for("get_chat_recipients"), headers={config.application.user_header_name: user_uid}
-    )
-
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert response.json() == {"detail": "User not found"}
 
 
 @pytest.mark.usefixtures("clear_db")
