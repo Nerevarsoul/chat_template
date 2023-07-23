3f5fe4833e8553b2567d89e3e9bc23791203d024
innokentiyzabirov <innokentiy.zabirov@exness.com>
2023-07-23 14:40:48 +0300

    add username to recipient in chat_list

--- tests/app/api/chats/utils.py
+++ tests/app/api/chats/utils.py
@@ -1,7 +1,8 @@
+from app import db
 from app.db.enums import ChatUserRole
 
 
-async def generate_chat_history(user_uid: str, chat_relationship_db_f, count: int = 1):
+async def generate_chat_history(user: db.User, chat_relationship_db_f, count: int = 1):
     for i in range(count):
-        rel = await chat_relationship_db_f.create(user__uid=user_uid, user_role=ChatUserRole.CREATOR)
-        await chat_relationship_db_f.create(chat__id=rel.chat_id)
+        rel = await chat_relationship_db_f.create(user=user, user_role=ChatUserRole.CREATOR)
+        await chat_relationship_db_f.create(chat=rel.chat)
