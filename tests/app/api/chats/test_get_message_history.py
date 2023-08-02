73b8e34c2c71025d575b45aabfb7ca3451c92d74
Georgii Alzhirov <alzh.george@gmail.com>
2023-08-02 13:54:08 +0000

    tests refactoring

--- tests/app/api/chats/test_get_message_history.py
+++ tests/app/api/chats/test_get_message_history.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import choice, randint
 from typing import TYPE_CHECKING
 
 import pytest
@@ -7,6 +7,7 @@ from fastapi import status
 from app import config
 from app.db.enums import ChatUserRole
 from app.main import app
+from tests.app.api.chats.utils import get_sorted_messages_id_list
 
 if TYPE_CHECKING:
     from httpx import AsyncClient
@@ -18,7 +19,7 @@ async def test_get_message_history_if_user_not_in_chat(
 ) -> None:
     user_1 = await user_db_f.create()
     chat_rel = await chat_relationship_db_f.create(user_role=ChatUserRole.CREATOR)
-    await message_db_f(user_uid=chat_rel.user_uid, chat_id=chat_rel.chat_id)
+    await message_db_f.create(user_uid=chat_rel.user_uid, chat_id=chat_rel.chat_id)
 
     response = await client.get(
         app.other_asgi_app.url_path_for("get_message_history"),
@@ -34,8 +35,9 @@ async def test_get_message_history_if_user_not_in_chat(
 async def test_get_message_history(client: "AsyncClient", chat_relationship_db_f, message_db_f) -> None:
     chat_rel = await chat_relationship_db_f.create(user_role=ChatUserRole.CREATOR)
     messages_count = randint(5, 15)
-    for _ in range(messages_count):
-        await message_db_f(user_uid=chat_rel.user_uid, chat_id=chat_rel.chat_id)
+    messages_id_list = await get_sorted_messages_id_list(
+        messages_count, chat_rel.user_uid, chat_rel.chat_id, message_db_f
+    )
 
     response = await client.get(
         app.other_asgi_app.url_path_for("get_message_history"),
@@ -48,9 +50,11 @@ async def test_get_message_history(client: "AsyncClient", chat_relationship_db_f
     assert response.status_code == status.HTTP_200_OK
     assert len(message_history) == messages_count
 
-    for message in message_history:
+    for message, message_id in zip(message_history, messages_id_list):
+        print(message["id"], message_id)
         assert message["user_uid"] == str(chat_rel.user_uid)
         assert message["chat_id"] == chat_rel.chat_id
+        assert message["id"] == message_id
 
 
 @pytest.mark.usefixtures("clear_db")
@@ -59,8 +63,9 @@ async def test_get_message_history_with_default_page_size_param(
 ) -> None:
     chat_rel = await chat_relationship_db_f.create(user_role=ChatUserRole.CREATOR)
     messages_count = config.application.message_history_page_size + 10
-    for i in range(1, messages_count + 1):
-        await message_db_f(id=i, user_uid=chat_rel.user_uid, chat_id=chat_rel.chat_id)
+    messages_id_list = await get_sorted_messages_id_list(
+        messages_count, chat_rel.user_uid, chat_rel.chat_id, message_db_f
+    )
 
     response = await client.get(
         app.other_asgi_app.url_path_for("get_message_history"),
@@ -73,7 +78,7 @@ async def test_get_message_history_with_default_page_size_param(
     assert response.status_code == status.HTTP_200_OK
     assert len(message_history) == config.application.message_history_page_size
 
-    for message, message_id in zip(message_history, range(1, config.application.message_history_page_size + 1)):
+    for message, message_id in zip(message_history, messages_id_list):
         assert message["user_uid"] == str(chat_rel.user_uid)
         assert message["chat_id"] == chat_rel.chat_id
         assert message["id"] == message_id
@@ -86,8 +91,9 @@ async def test_get_message_history_with_page_size_param(
     chat_rel = await chat_relationship_db_f.create(user_role=ChatUserRole.CREATOR)
     messages_count = 20
     page_size = randint(5, 15)
-    for i in range(1, messages_count + 1):
-        await message_db_f(id=i, user_uid=chat_rel.user_uid, chat_id=chat_rel.chat_id)
+    messages_id_list = await get_sorted_messages_id_list(
+        messages_count, chat_rel.user_uid, chat_rel.chat_id, message_db_f
+    )
 
     response = await client.get(
         app.other_asgi_app.url_path_for("get_message_history"),
@@ -100,7 +106,7 @@ async def test_get_message_history_with_page_size_param(
     assert response.status_code == status.HTTP_200_OK
     assert len(message_history) == page_size
 
-    for message, message_id in zip(message_history, range(1, page_size + 1)):
+    for message, message_id in zip(message_history, messages_id_list):
         assert message["user_uid"] == str(chat_rel.user_uid)
         assert message["chat_id"] == chat_rel.chat_id
         assert message["id"] == message_id
@@ -111,10 +117,12 @@ async def test_get_message_history_with_message_id_param(
     client: "AsyncClient", chat_relationship_db_f, message_db_f
 ) -> None:
     chat_rel = await chat_relationship_db_f.create(user_role=ChatUserRole.CREATOR)
-    messages_count = config.application.message_history_page_size
-    message_id = randint(1, messages_count)
-    for i in range(1, messages_count + 1):
-        await message_db_f(id=i, user_uid=chat_rel.user_uid, chat_id=chat_rel.chat_id)
+    messages_count = config.application.message_history_page_size * 2
+    messages_id_list = await get_sorted_messages_id_list(
+        messages_count, chat_rel.user_uid, chat_rel.chat_id, message_db_f
+    )
+    message_index = config.application.message_history_page_size
+    message_id = messages_id_list[message_index]
 
     response = await client.get(
         app.other_asgi_app.url_path_for("get_message_history"),
@@ -125,9 +133,9 @@ async def test_get_message_history_with_message_id_param(
     message_history = response.json()
 
     assert response.status_code == status.HTTP_200_OK
-    assert len(message_history) == messages_count - message_id + 1
+    assert len(message_history) == config.application.message_history_page_size
 
-    for message, message_id in zip(message_history, range(message_id, messages_count + 1)):
+    for message, message_id in zip(message_history, messages_id_list[message_index:]):
         assert message["user_uid"] == str(chat_rel.user_uid)
         assert message["chat_id"] == chat_rel.chat_id
         assert message["id"] == message_id
