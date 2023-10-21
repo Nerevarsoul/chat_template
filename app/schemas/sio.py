934bf5588186ae23c2544af4682abf3dc95c1776
Georgii Alzhirov <alzh.george@gmail.com>
2023-10-21 14:05:44 +0000

    inherit EditMessageData from NewMessage

--- app/schemas/sio.py
+++ app/schemas/sio.py
@@ -24,16 +24,5 @@ class NewMessage(BaseModel):
         return clear_text
 
 
-class EditMessageData(BaseModel):
-    user_uid: UUID4 = Field(alias="sender_id")
+class EditMessageData(NewMessage):
     message_id: int
-    chat_id: int
-    client_id: UUID4
-    text: str
-
-    @field_validator("text")
-    def edited_message_contains_only_spaces(cls, text: str) -> str:
-        clear_text = text.strip()
-        if not clear_text:
-            raise ValueError("text must contain characters")
-        return clear_text
